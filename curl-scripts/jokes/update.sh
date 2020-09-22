82ca2ce58ecb5aac765fc7b3b8e51d7440367eba
Riadmourad <riyad.mourad@gmail.com>
2020-09-22 16:41:04 -0400

    Update curl

--- curl-scripts/jokes/update.sh
+++ curl-scripts/jokes/update.sh
@@ -10,7 +10,8 @@ curl "${API}${URL_PATH}/${ID}" \
 --header "Authorization: Bearer ${TOKEN}" \
 --data '{
     "joke": {
-      "text": "'"${TEXT}"'"
+      "text": "'"${TEXT}"'",
+      "title": "'"${TITLE}"'"
     }
   }'
 
